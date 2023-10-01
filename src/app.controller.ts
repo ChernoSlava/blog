60afe0290f66090f4619c426148b0833258bb572
Viacheslav Chernogorov <slavachernogorv@mail.ru>
2023-10-01 23:35:25 +0300

    feat: two pages
    create two base pages and use route.
    
    meta: issue-1

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,18 +1,15 @@
 import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
 import { articles } from './article';
 import { Render, Param, ParseIntPipe } from '@nestjs/common';
 @Controller()
 export class AppController {
-  // constructor(private readonly appService: AppService) {}
-
   @Get()
   @Render('index')
   index() {
     return { articles };
   }
 
-  @Get('articles/:id')
+  @Get(':id')
   @Render('article')
   getById(@Param('id', ParseIntPipe) id: number) {
     return articles.find((article) => article.id === id);
