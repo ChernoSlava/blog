1bc905a497f0af3ea7b45eeced814d08425c7964
Viacheslav Chernogorov <slavachernogorv@mail.ru>
2023-10-02 00:25:19 +0300

    feat: base styles
    add base styles.
    
    meta: issue-2

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,16 @@ import { join } from 'path';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as express from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
   app.setBaseViewsDir(join(__dirname, '../src/views'));
+  app.use(express.static('public'));
 
   app.setViewEngine('pug');
+
   await app.listen(3000);
 }
 
